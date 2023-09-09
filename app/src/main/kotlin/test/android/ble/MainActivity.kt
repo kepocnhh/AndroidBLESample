c63a2374563462a0b30255d23672fde8b63ba666
Stanley Wintergreen <kepocnhh@gmail.com>
2023-09-09 15:39:52 +0300

    compose 1.4.3

--- app/src/main/kotlin/test/android/ble/MainActivity.kt
+++ app/src/main/kotlin/test/android/ble/MainActivity.kt
@@ -1,11 +1,18 @@
 package test.android.ble
 
 import android.os.Bundle
+import androidx.activity.compose.BackHandler
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 
 internal class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // todo
+        setContent {
+            BackHandler {
+                finish()
+            }
+            // todo
+        }
     }
 }
--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -1,11 +1,11 @@
 object Version {
     const val jvmTarget = "11"
-    const val kotlin = "1.8.22"
+    const val kotlin = "1.8.10"
 
     object Android {
-        const val compileSdk = 33
+        const val compileSdk = 34
         const val minSdk = 24
         const val targetSdk = compileSdk
-        const val compose = "1.4.1"
+        const val compose = "1.4.3" // kotlin 1.8.10
     }
 }
