863d9c0bfd0e072f80567517f3a9b707581b4a60
Stanley Wintergreen <kepocnhh@gmail.com>
2023-09-09 16:11:54 +0300

    scan state

--- app/src/main/kotlin/test/android/ble/module/scanner/ScannerScreen.kt
+++ app/src/main/kotlin/test/android/ble/module/scanner/ScannerScreen.kt
@@ -1,17 +1,59 @@
 package test.android.ble.module.scanner
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import test.android.ble.util.compose.toPaddings
 
 @Composable
 internal fun ScannerScreen() {
-    Box(
+    val insets = LocalView.current.rootWindowInsets.toPaddings()
+    Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Red),
-    )
+            .background(Color.White)
+            .padding(insets),
+    ) {
+        val scanState = remember { mutableStateOf(false) }
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+        ) {
+            // todo
+        }
+        BasicText(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(64.dp)
+                .clickable {
+                    // todo
+                }
+                .wrapContentSize(),
+            text = "foo",
+            style = TextStyle(
+                textAlign = TextAlign.Center,
+                color = Color.Black,
+                fontSize = 16.sp,
+            ),
+        )
+    }
 }
