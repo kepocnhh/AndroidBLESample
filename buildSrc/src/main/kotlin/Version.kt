9537f244df6cfc170aab2ceffc2dba3275f7e862
Stanley Wintergreen <kepocnhh@gmail.com>
2023-09-09 15:15:06 +0300

    init gradle

---
+++ buildSrc/src/main/kotlin/Version.kt
@@ -0,0 +1,11 @@
+object Version {
+    const val jvmTarget = "11"
+    const val kotlin = "1.8.22"
+
+    object Android {
+        const val compileSdk = 33
+        const val minSdk = 24
+        const val targetSdk = compileSdk
+        const val compose = "1.4.1"
+    }
+}
