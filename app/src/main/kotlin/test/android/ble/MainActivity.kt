e96885b919ddc77fd339fc0284d0704db124e699
Stanley Wintergreen <kepocnhh@gmail.com>
2023-09-19 11:57:08 +0300

    ACCESS_BACKGROUND_LOCATION

--- app/src/main/kotlin/test/android/ble/MainActivity.kt
+++ app/src/main/kotlin/test/android/ble/MainActivity.kt
@@ -2,8 +2,11 @@ package test.android.ble
 
 import android.Manifest
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
@@ -12,50 +15,56 @@ import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 import test.android.ble.module.router.RouterScreen
 import test.android.ble.util.android.isAllGranted
 import test.android.ble.util.android.isGranted
 import test.android.ble.util.android.notGranted
 import test.android.ble.util.android.showToast
+import test.android.ble.util.compose.PermissionsRequester
 import test.android.ble.util.compose.isAllRequested
-import test.android.ble.util.compose.permissionsRequester
 
 internal class MainActivity : AppCompatActivity() {
     companion object {
         private const val TAG = "[Main]"
     }
 
-    private val permissionsRequester = permissionsRequester()
+    private val permissionsRequester = PermissionsRequester(this)
 
     @RequiresApi(Build.VERSION_CODES.R)
     @Composable
     private fun LocationGranted() {
+        Log.d(TAG, "location granted...")
+        val context = LocalContext.current
         val permissions = arrayOf(
             Manifest.permission.ACCESS_BACKGROUND_LOCATION,
         )
         if (isAllGranted(permissions)) {
             RouterScreen()
         } else if (permissionsRequester.collectAsState().isAllRequested(permissions)) {
-            val context = LocalContext.current
             showToast("No ${permissions.notGranted(context)} permissions!")
             finish()
         } else {
             val permission = Manifest.permission.ACCESS_BACKGROUND_LOCATION
             if (shouldShowRequestPermissionRationale(permission)) {
+                Log.d(TAG, "should show request permission $permission rationale...")
                 val label = packageManager.backgroundPermissionOptionLabel
                     .takeIf { it.isNotEmpty() }
                     ?: "Background permission option label." // todo
                 AlertDialog
                     .Builder(this)
+                    .setCancelable(false)
                     .setTitle(label)
                     .setMessage("Need $permission permission.")
-                    .setPositiveButton("to settings") { _, _ ->
-                        permissionsRequester.request(permissions)
+                    .setPositiveButton("to permission") { _,  _ ->
+                        permissionsRequester.request(arrayOf(permission))
                     }
                     .show()
             } else {
-                permissionsRequester.request(permissions)
+                Log.d(TAG, "request permission $permission...")
+                permissionsRequester.request(arrayOf(permission))
             }
         }
     }
