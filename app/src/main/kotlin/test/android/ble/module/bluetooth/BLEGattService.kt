9f4239dcf6dbd9b7cd1e864cc345620cbfb2f691
Stanley Wintergreen <kepocnhh@gmail.com>
2023-09-19 18:17:42 +0300

    gatt connect

---
+++ app/src/main/kotlin/test/android/ble/module/bluetooth/BLEGattService.kt
@@ -0,0 +1,154 @@
+package test.android.ble.module.bluetooth
+
+import android.app.Service
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothGatt
+import android.bluetooth.BluetoothGattCallback
+import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothProfile
+import android.content.Context
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+internal class BLEGattException(val error: BLEGattService.Error) : Exception()
+
+internal class BLEGattService : Service() {
+    sealed interface Broadcast {
+        class OnError(val error: Error?) : Broadcast
+    }
+
+    enum class ConnectState {
+        NONE,
+        CONNECTED,
+        DISCONNECTED,
+    }
+
+    enum class Error {
+        BT_NO_ADAPTER,
+        BT_NO_PERMISSION,
+        BT_ADAPTER_DISABLED,
+        BT_NO_CONNECT_PERMISSION,
+    }
+
+    private val job = SupervisorJob()
+    private val scope = CoroutineScope(Dispatchers.Main + job)
+
+    private val callback = object : BluetoothGattCallback() {
+        override fun onConnectionStateChange(gatt: BluetoothGatt?, status: Int, newState: Int) {
+            if (gatt == null) return // todo
+            Log.d(TAG, "on connection state change $status $newState")
+            when (status) {
+                BluetoothGatt.GATT_SUCCESS -> {
+                    when (newState) {
+                        BluetoothProfile.STATE_CONNECTED -> {
+                            _connectState.value = ConnectState.CONNECTED
+                        }
+                        BluetoothProfile.STATE_DISCONNECTED -> {
+                            _connectState.value = ConnectState.DISCONNECTED
+                        }
+                        else -> {
+                            TODO("Gatt newState: $newState")
+                        }
+                    }
+                }
+                else -> {
+                    TODO("Gatt status: $status")
+                }
+            }
+        }
+    }
+
+    private fun onConnect(address: String) {
+        Log.d(TAG, "connect $address...")
+        if (connectState.value != ConnectState.DISCONNECTED) TODO("connect state: ${connectState.value}")
+        val context: Context = this
+        scope.launch {
+            _connectState.value = ConnectState.NONE
+            runCatching {
+                val adapter = getBluetoothAdapter(context)
+                withContext(Dispatchers.Default) {
+                    val device = adapter.getRemoteDevice(address)
+                    Log.d(TAG, "device: $device")
+                    val autoConnect = false
+                    try {
+                        device.connectGatt(context, autoConnect, callback, BluetoothDevice.TRANSPORT_LE)
+                    } catch (e: SecurityException) {
+                        throw BLEGattException(Error.BT_NO_CONNECT_PERMISSION)
+                    }
+                }
+            }.fold(
+                onSuccess = {
+                    // todo bt on/off
+                },
+                onFailure = {
+                    val error = (it as? BLEGattException)?.error
+                    _broadcast.emit(Broadcast.OnError(error))
+                    _connectState.value = ConnectState.DISCONNECTED
+                }
+            )
+        }
+    }
+
+    private fun onStartCommand(intent: Intent) {
+        when (intent.action) {
+            ACTION_CONNECT -> onConnect(address = intent.getStringExtra("address")!!)
+        }
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if (intent != null) onStartCommand(intent)
+        return START_NOT_STICKY
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    companion object {
+        private const val TAG = "[Gatt]"
+        val ACTION_CONNECT = "${this::class.java.name}:ACTION_CONNECT"
+        val ACTION_DISCONNECT = "${this::class.java.name}:ACTION_CONNECT"
+
+        private val _broadcast = MutableSharedFlow<Broadcast>()
+        val broadcast = _broadcast.asSharedFlow()
+        private val _connectState = MutableStateFlow(ConnectState.DISCONNECTED)
+        val connectState = _connectState.asStateFlow()
+
+        fun connect(context: Context, address: String) {
+            val intent = Intent(context, BLEGattService::class.java)
+            intent.action = ACTION_CONNECT
+            intent.putExtra("address", address)
+            context.startService(intent)
+        }
+
+        fun disconnect(context: Context) {
+            val intent = Intent(context, BLEGattService::class.java)
+            intent.action = ACTION_DISCONNECT
+            context.startService(intent)
+        }
+
+        private fun getBluetoothAdapter(context: Context): BluetoothAdapter {
+            val bluetoothManager = context.getSystemService(BluetoothManager::class.java)
+            val adapter: BluetoothAdapter = bluetoothManager.adapter
+                ?: throw BLEGattException(Error.BT_NO_ADAPTER)
+            val isEnabled = try {
+                adapter.isEnabled
+            } catch (e: SecurityException) {
+                throw BLEGattException(Error.BT_NO_PERMISSION)
+            }
+            if (!isEnabled) throw BLEGattException(Error.BT_ADAPTER_DISABLED)
+            return adapter
+        }
+    }
+}
--- app/src/main/kotlin/test/android/ble/module/device/DeviceScreen.kt
+++ app/src/main/kotlin/test/android/ble/module/device/DeviceScreen.kt
@@ -3,22 +3,31 @@ package test.android.ble.module.device
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import test.android.ble.module.bluetooth.BLEGattService
+import test.android.ble.util.android.showToast
 import test.android.ble.util.compose.toPaddings
 
 @Composable
@@ -26,7 +35,24 @@ internal fun DeviceScreen(
     address: String,
     onForget: () -> Unit,
 ) {
+    val context = LocalContext.current
     val insets = LocalView.current.rootWindowInsets.toPaddings()
+    val connectState by BLEGattService.connectState.collectAsState()
+    LaunchedEffect(Unit) {
+        BLEGattService.broadcast.collect { broadcast ->
+            when (broadcast) {
+                is BLEGattService.Broadcast.OnError -> {
+                    when (broadcast.error) {
+                        BLEGattService.Error.BT_NO_ADAPTER -> context.showToast("No adapter!")
+                        BLEGattService.Error.BT_NO_PERMISSION -> context.showToast("No permission!")
+                        BLEGattService.Error.BT_ADAPTER_DISABLED -> context.showToast("Adapter disabled!")
+                        BLEGattService.Error.BT_NO_CONNECT_PERMISSION -> context.showToast("No connect permission!")
+                        null -> context.showToast("Unknown error!")
+                    }
+                }
+            }
+        }
+    }
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -46,19 +72,66 @@ internal fun DeviceScreen(
                 fontFamily = FontFamily.Monospace,
             ),
         )
-        BasicText(
-            modifier = Modifier
-                .align(Alignment.BottomCenter)
-                .fillMaxWidth()
-                .height(64.dp)
-                .clickable(onClick = onForget)
-                .wrapContentHeight(),
-            text = "forget",
-            style = TextStyle(
-                textAlign = TextAlign.Center,
-                color = Color.Black,
-                fontSize = 16.sp,
-            ),
+        val textStyle = TextStyle(
+            textAlign = TextAlign.Center,
+            color = Color.Black,
+            fontSize = 16.sp,
         )
+        when (connectState) {
+            BLEGattService.ConnectState.NONE -> {
+                BasicText(
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .fillMaxWidth()
+                        .height(64.dp)
+                        .wrapContentHeight(),
+                    text = "...",
+                    style = textStyle,
+                )
+            }
+            BLEGattService.ConnectState.CONNECTED -> {
+                BasicText(
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .fillMaxWidth()
+                        .height(64.dp)
+                        .clickable {
+                            TODO()
+                        }
+                        .wrapContentHeight(),
+                    text = "disconnect",
+                    style = textStyle,
+                )
+            }
+            BLEGattService.ConnectState.DISCONNECTED -> {
+                Row(
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .fillMaxWidth()
+                        .height(64.dp),
+                ) {
+                    BasicText(
+                        modifier = Modifier
+                            .weight(1f)
+                            .fillMaxHeight()
+                            .clickable {
+                                BLEGattService.connect(context, address = address)
+                            }
+                            .wrapContentSize(),
+                        text = "connect",
+                        style = textStyle,
+                    )
+                    BasicText(
+                        modifier = Modifier
+                            .weight(1f)
+                            .fillMaxHeight()
+                            .clickable(onClick = onForget)
+                            .wrapContentSize(),
+                        text = "forget",
+                        style = textStyle,
+                    )
+                }
+            }
+        }
     }
 }
