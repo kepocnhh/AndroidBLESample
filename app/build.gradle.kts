bf5cbf0a7a41f2075bcea163851be7f577028a4f
Stanley Wintergreen <kepocnhh@gmail.com>
2023-10-06 16:19:06 +0300

    0.0.15-15

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = appId
         minSdk = Version.Android.minSdk
         targetSdk = Version.Android.targetSdk
-        versionCode = 14
+        versionCode = 15
         versionName = "0.0.$versionCode"
         manifestPlaceholders["appName"] = "@string/app_name"
     }
