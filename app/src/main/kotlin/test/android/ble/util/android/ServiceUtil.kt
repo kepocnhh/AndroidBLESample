1c70e797db405938d733cfdf5df08f0326093b48
Stanley Wintergreen <kepocnhh@gmail.com>
2023-10-10 22:28:36 +0300

    start/stop foreground...

---
+++ app/src/main/kotlin/test/android/ble/util/android/ServiceUtil.kt
@@ -0,0 +1,65 @@
+package test.android.ble.util.android
+
+import android.app.Notification
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+
+internal object ServiceUtil {
+    val ACTION_START_FOREGROUND = "${this::class.java.name}:ACTION_START_FOREGROUND"
+    val ACTION_STOP_FOREGROUND = "${this::class.java.name}:ACTION_STOP_FOREGROUND"
+
+    fun getNotificationId(intent: Intent): Int {
+        if (!intent.hasExtra("notificationId")) TODO()
+        return intent.getIntExtra("notificationId", -1)
+    }
+
+    fun getNotification(intent: Intent): Notification {
+        if (!intent.hasExtra("notification")) TODO()
+        return intent.getParcelableExtra("notification") ?: TODO()
+    }
+
+    fun getNotificationBehavior(intent: Intent): Int {
+        if (!intent.hasExtra("notificationBehavior")) TODO()
+        val result = intent.getIntExtra("notificationBehavior", -1)
+        when (result) {
+            Service.STOP_FOREGROUND_REMOVE -> {
+                // noop
+            }
+            else -> TODO()
+        }
+        return result
+    }
+
+    @JvmStatic
+    fun <T : Service> startForeground(
+        context: Context,
+        type: Class<T>,
+        notificationId: Int,
+        notification: Notification,
+    ) {
+        val intent = Intent(context, type)
+        intent.action = ACTION_START_FOREGROUND
+        intent.putExtra("notificationId", notificationId)
+        intent.putExtra("notification", notification)
+        context.startService(intent)
+    }
+
+    @JvmStatic
+    fun <T : Service> stopForeground(
+        context: Context,
+        type: Class<T>,
+        notificationBehavior: Int,
+    ) {
+        when (notificationBehavior) {
+            Service.STOP_FOREGROUND_REMOVE -> {
+                // noop
+            }
+            else -> TODO()
+        }
+        val intent = Intent(context, type)
+        intent.action = ACTION_STOP_FOREGROUND
+        intent.putExtra("notificationBehavior", notificationBehavior)
+        context.startService(intent)
+    }
+}
